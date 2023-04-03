5688da6d22042c81fed18136ba6ff4bc99291d28
Mateo Kurti <contact@mateokurti.com>
2023-04-03 23:44:57 +0200

    Update sign in form

--- app/Views/auth/sign_in.php
+++ app/Views/auth/sign_in.php
@@ -17,7 +17,8 @@
     <div class="py-6 w-full">
     <div class="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
             <div class="hidden lg:block lg:w-1/2 bg-cover" style="background-image:url('/assets/img/courier.jpg')"></div>
-            <div class="w-full p-8 lg:w-1/2">
+            <!-- <div class="w-full p-8 lg:w-1/2"> -->
+            <form class="w-full p-8 lg:w-1/2" action="/sign-in" method="POST">
                 <p class="text-xl text-gray-600 text-center">Welcome back!</p>
                 <a href="#" class="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100">
                     <div class="px-4 py-3">
@@ -37,24 +38,25 @@
                 </div>
                 <div class="mt-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
-                    <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email" placeholder="hello@fshn.edu.al">
+                    <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email" name="email" placeholder="hello@fshn.edu.al">
                 </div>
                 <div class="mt-4">
                     <div class="flex justify-between">
                         <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
                         <a href="#" class="text-xs text-gray-500">Forget Password?</a>
                     </div>
-                    <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" placeholder="••••••••••">
+                    <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" name="password" placeholder="••••••••••">
                 </div>
                 <div class="mt-8">
-                    <button class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Login</button>
+                    <button type="submit" class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Login</button>
                 </div>
                 <div class="mt-4 flex items-center justify-between">
                     <span class="border-b w-1/5 md:w-1/4"></span>
                     <a href="#" class="text-xs text-gray-500 uppercase">or sign up</a>
                     <span class="border-b w-1/5 md:w-1/4"></span>
                 </div>
-            </div>
+            </form>
+            <!-- </div> -->
         </div>
     </div>
 </div>
