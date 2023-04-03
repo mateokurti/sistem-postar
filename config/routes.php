075179b0912bc50d9ded9a4ce82f54524a85517a
Igli <iglikoxha@outlook.com>
2023-04-04 00:32:54 +0200

    Google SSO

--- config/routes.php
+++ config/routes.php
@@ -5,3 +5,5 @@ $router->post('sign-up', 'AuthController@signUp');
 $router->get('sign-out', 'AuthController@signOut');
 
 $router->get('dashboard', 'DashboardController@index');
+
+$router->get('oauth/google', 'AuthController@googleSignIn');
