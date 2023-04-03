d00ac9bf8926e7119ef83ae4567bbc39ef790ba3
Mateo Kurti <contact@mateokurti.com>
2023-04-03 23:59:40 +0200

    Update imports on dashboard

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,6 +1,5 @@
-<?php include '../common/head.php';?>
+<?php include __DIR__ . '/../common/head.php'; ?>
     <h1>Welcome, <?php echo $identity['first_name']; ?>!</h1>
     <p>You are logged in as <?php echo $identity['email']; ?>.</p>
     <p><a href="/sign-out">Logout</a></p>
-<?php include '../common/footer.php';?>
-
+<?php include __DIR__ . '/../common/footer.php'; ?>
