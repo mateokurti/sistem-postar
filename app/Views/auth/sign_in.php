11b7e5dfdb2c310a872ef3702ba760a4e5782189
Mateo Kurti <contact@mateokurti.com>
2023-04-04 09:16:18 +0200

    Add sign up form

--- app/Views/auth/sign_in.php
+++ app/Views/auth/sign_in.php
@@ -10,8 +10,8 @@
 <div class="flex items-center justify-center h-screen relative z-10">
     <div class="py-6 w-full">
         <div class="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
-        <div class="hidden lg:block lg:w-1/2 bg-cover" style="background-image:url('/assets/img/courier.jpg')"></div>
-            <form class="w-full p-8 lg:w-1/2" action="/sign-in" method="POST">
+            <div class="hidden lg:block lg:w-1/2 bg-cover" style="background-image:url('/assets/img/courier.jpg')"></div>
+            <div class="w-full p-8 lg:w-1/2">
                 <p class="text-xl text-gray-600 text-center">Welcome back!</p>
                 <a href="<?php echo $googleAuthUrl; ?>" class="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100">
                     <div class="px-4 py-3">
@@ -22,41 +22,96 @@
                             <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2"/>
                         </svg>
                     </div>
-                    <h1 class="px-4 py-3 w-5/6 text-center text-gray-600 font-bold">Sign in with Google</h1>
+                    <h1 class="px-4 py-3 w-5/6 text-center text-gray-600 font-bold">Continue with Google</h1>
                 </a>
-                <div class="mt-4 flex items-center justify-between">
-                    <span class="border-b w-1/5 lg:w-1/4"></span>
-                    <a href="#" class="text-xs text-center text-gray-500 uppercase">or login with email</a>
-                    <span class="border-b w-1/5 lg:w-1/4"></span>
-                </div>
-            <div class="mt-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
-                <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email" name="email" placeholder="hello@fshn.edu.al">
-                </div>
-                <div class="mt-4">
-                    <div class="flex justify-between">
-                        <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                        <a href="#" class="text-xs text-gray-500">Forget Password?</a>
+                <form id="sign-in-form" action="/sign-in" method="POST">
+                    <div class="mt-4 flex items-center justify-between">
+                        <span class="border-b w-1/5 lg:w-1/4"></span>
+                        <a href="#" class="text-xs text-center text-gray-500 uppercase">or login with email</a>
+                        <span class="border-b w-1/5 lg:w-1/4"></span>
                     </div>
-                    <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" name="password" placeholder="••••••••••">
-                </div>
-                <?php if (isset($_SESSION['flash']['error'])): ?>
-                <div class="mt-4">
-                    <p class="text-red-500 text-xs italic" id="form-warning"><?= $_SESSION['flash']['error']; ?></p>
-                </div>
-                <?php endif; ?>
-                <div class="mt-8">
-                <button type="submit" class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Sign In</button>
-                </div>
-                <div class="mt-4 flex items-center justify-between">
-                    <span class="border-b w-1/5 md:w-1/4"></span>
-                    <a href="#" class="text-xs text-gray-500 uppercase">or sign up</a>
-                    <span class="border-b w-1/5 md:w-1/4"></span>
-                </div>
-            </form>
+                    <div class="mt-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
+                        <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email" name="email" placeholder="hello@fshn.edu.al">
+                    </div>
+                    <div class="mt-4">
+                        <div class="flex justify-between">
+                            <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                            <a href="#" class="text-xs text-gray-500">Forget Password?</a>
+                        </div>
+                        <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" name="password" placeholder="••••••••••">
+                    </div>
+                    <?php if (isset($_SESSION['flash']['error'])): ?>
+                    <div class="mt-4">
+                        <p class="text-red-500 text-xs italic" id="form-warning"><?= $_SESSION['flash']['error']; ?></p>
+                    </div>
+                    <?php endif; ?>
+                    <div class="mt-8">
+                    <button type="submit" class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Sign In</button>
+                    </div>
+                    <div class="mt-4 flex items-center justify-between">
+                        <span class="border-b w-1/5 md:w-1/4"></span>
+                        <a id="show-sign-up" href="#" class="text-xs text-gray-500 uppercase">or sign up</a>
+                        <span class="border-b w-1/5 md:w-1/4"></span>
+                    </div>
+                </form>
+                <form id="sign-up-form" class="hidden" action="/sign-up" method="POST">
+                    <div class="mt-4 flex items-center justify-between">
+                        <span class="border-b w-1/5 lg:w-1/4"></span>
+                        <a href="#" class="text-xs text-center text-gray-500 uppercase">or sign up with email</a>
+                        <span class="border-b w-1/5 lg:w-1/4"></span>
+                    </div>
+                    <div class="mt-4">
+                        <div class="flex justify-between">
+                            <div class="w-1/2 mr-1">
+                                <label class="block text-gray-700 text-sm font-bold mb-2">First Name</label>
+                                <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="first_name" name="first_name" placeholder="Filan">
+                            </div>
+                            <div class="w-1/2 ml-1">
+                                <label class="block text-gray-700 text-sm font-bold mb-2">Last Name</label>
+                                <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="last_name" name="last_name" placeholder="Fisteku">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
+                        <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email" name="email" placeholder="hello@fshn.edu.al">
+                    </div>
+                    <div class="mt-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                        <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" name="password" placeholder="••••••••••">
+                    </div>
+                    <?php if (isset($_SESSION['flash']['error'])): ?>
+                    <div class="mt-4">
+                        <p class="text-red-500 text-xs italic" id="form-warning"><?= $_SESSION['flash']['error']; ?></p>
+                    </div>
+                    <?php endif; ?>
+                    <div class="mt-8">
+                    <button type="submit" class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Sign Up</button>
+                    </div>
+                    <div class="mt-4 flex items-center justify-between">
+                        <span class="border-b w-1/5 md:w-1/4"></span>
+                        <a id="show-sign-in" href="#" class="text-xs text-gray-500 uppercase">or sign in</a>
+                        <span class="border-b w-1/5 md:w-1/4"></span>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
 
+<script>
+    document.getElementById('show-sign-in').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('sign-up-form').classList.add('hidden');
+        document.getElementById('sign-in-form').classList.remove('hidden');
+    });
+
+    document.getElementById('show-sign-up').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('sign-in-form').classList.add('hidden');
+        document.getElementById('sign-up-form').classList.remove('hidden');
+    });
+</script>
 
 <?php include __DIR__ . '/../common/footer.php'; ?>
