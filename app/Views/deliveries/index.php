2cac8e75c20fe6ea89a50ef73c73395fe7c08edb
Mateo Kurti <contact@mateokurti.com>
2023-05-02 00:04:14 +0200

    Add tracking history modal

--- app/Views/deliveries/index.php
+++ app/Views/deliveries/index.php
@@ -6,6 +6,7 @@
 
 
 <?php include __DIR__ . '/create.php'; ?>
+<?php include __DIR__ . '/tracking-history.php'; ?>
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg my-4">
     <div class="flex items-center justify-between pb-4 bg-white dark:bg-gray-900">
@@ -68,76 +69,81 @@
             <?php
               foreach ($deliveries as $row) {
             ?>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="w-4 p-4">
-                    <div class="flex items-center">
-                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                    </div>
-                </td>
-                <td class="px-6 py-4">
-                    <?= $row['tracking_number'] ?>
-                </td>
-                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                    <div>
-                        <div class="text-base font-semibold"><?= $row['recipient']['first_name'] . ' ' .  $row['recipient']['last_name'] ?></div>
-                        <div class="font-normal text-gray-500"><?= $row['recipient']['email'] ?></div>
-                    </div>  
-                </th>
-                <td class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                    <div>
-                        <div class="text-base font-normal"><?= $row['address']['street'] ?></div>
-                        <div class="font-normal text-gray-500"><?= $row['address']['city'] . ' ' . $row['address']['zip'] ?></div>
-                    </div>  
-                </td>
-                
-                <td class="px-6 py-4">
-                    <div class="flex items-center">
-                        <div class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> S'ka Informacion
-                    </div>
-                </td>
-                <?php
-                $holder_title = "S'ka informacion";
-                $holder_subtitle = "";
-                if ($row['holder']['type'] == 'user') {
-                  $holder_title = $row['holder']['first_name'] . ' ' . $row['holder']['last_name'];
-                  if ($row['holder']['id'] == $identity['id']) {
-                    $holder_subtitle = 'Pakon e ke ende ti';
-                  } else if ($row['holder']['id'] == $row['sender']['id']) {
-                    $holder_subtitle = 'Pakon e ka ende dërguesi';
-                  } else if ($row['holder']['id'] == $row['recipient']['id']) {
-                    $holder_subtitle = 'Pakon e ka marrë marrësi';
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                  <td class="w-4 p-4">
+                      <div class="flex items-center">
+                          <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                          <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                      </div>
+                  </td>
+                  <td class="px-6 py-4">
+                      <?= $row['tracking_number'] ?>
+                  </td>
+                  <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                      <div>
+                          <div class="text-base font-semibold"><?= $row['recipient']['first_name'] . ' ' .  $row['recipient']['last_name'] ?></div>
+                          <div class="font-normal text-gray-500"><?= $row['recipient']['email'] ?></div>
+                      </div>  
+                  </th>
+                  <td class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                      <div>
+                          <div class="text-base font-normal"><?= $row['address']['street'] ?></div>
+                          <div class="font-normal text-gray-500"><?= $row['address']['city'] . ' ' . $row['address']['zip'] ?></div>
+                      </div>  
+                  </td>
+                  
+                  <td class="px-6 py-4">
+                      <div class="flex items-center">
+                          <div class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> S'ka Informacion
+                      </div>
+                  </td>
+                  <?php
+                  $holder_title = "S'ka informacion";
+                  $holder_subtitle = "";
+                  if ($row['holder']['type'] == 'user') {
+                    $holder_title = $row['holder']['first_name'] . ' ' . $row['holder']['last_name'];
+                    if ($row['holder']['id'] == $identity['id']) {
+                      $holder_subtitle = 'Pakon e ke ende ti';
+                    } else if ($row['holder']['id'] == $row['sender']['id']) {
+                      $holder_subtitle = 'Pakon e ka ende dërguesi';
+                    } else if ($row['holder']['id'] == $row['recipient']['id']) {
+                      $holder_subtitle = 'Pakon e ka marrë marrësi';
+                    }
+                  } else if ($row['holder']['type'] == 'courier') {
+                    $holder_title = $row['holder']['first_name'] . ' ' . $row['holder']['last_name'];
+                    $holder_subtitle = 'Pakon e ka korrieri';
+                  } else if ($row['holder']['type'] == 'office') {
+                    $holder_title = $row['holder']['name'];
+                    $holder_subtitle = 'Pakoja në zyrë postare';
                   }
-                } else if ($row['holder']['type'] == 'courier') {
-                  $holder_title = $row['holder']['first_name'] . ' ' . $row['holder']['last_name'];
-                  $holder_subtitle = 'Pakon e ka korrieri';
-                } else if ($row['holder']['type'] == 'office') {
-                  $holder_title = $row['holder']['name'];
-                  $holder_subtitle = 'Pakoja në zyrë postare';
-                }
-                ?>
-                <td class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                    <div>
-                        <div class="text-base font-normal"><?= $holder_title ?></div>
-                        <div class="font-normal text-gray-500"><?= $holder_subtitle ?></div>
-                    </div>  
-                </td>
-                <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                      </svg>
-                    </a>
-                </td>
-            </tr>
+                  ?>
+                  <td class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                      <div>
+                          <div class="text-base font-normal"><?= $holder_title ?></div>
+                          <div class="font-normal text-gray-500"><?= $holder_subtitle ?></div>
+                      </div>  
+                  </td>
+                  <td scope="row" class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center">
+                      <a data-modal-target="tracking-history-modal" data-modal-toggle="tracking-history-modal" id="open-tracking-modal" href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+                        </svg>
+                      </a>
+                      <a href="#" class="pl-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                        </svg>
+                      </a>
+                      </div>
+                  </td>
+              </tr>
             <?php
               }
             ?>
         </tbody>
     </table>
 </div>
-
-
 
 <script>
   const searchInput = document.querySelector('#table-search-delivery');
---
+++ app/Views/deliveries/tracking-history.php
@@ -0,0 +1,57 @@
+
+<div id="tracking-history-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-2xl max-h-full">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Gjurmimi i Dërgesës
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="tracking-history-modal">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Ora
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Përshkrimi
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Vendodhja e Pakos
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                23/04/2023 12:34:14
+                            </th>
+                            <td class="px-6 py-4">
+                                Pakoja u Krijua me Sukses
+                            </td>
+                            <td class="px-6 py-4">
+                                Filan Fisteku (Dërguesi)
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                23/04/2023 18:22:15
+                            </th>
+                            <td class="px-6 py-4">
+                                Pakoja u mor nga Korrieri
+                            </td>
+                            <td class="px-6 py-4">
+                                Filan Fisteku (Korrieri)
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
